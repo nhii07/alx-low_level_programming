f36fe1fe0dfe55fe88d657532de97a5c234aa16a
nhii07 <stannan3107@gmail.com>
2023-03-17 21:52:17 -0700

    s

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - prints all possible combinations of two two-digit numbers
- * Return: Always 0 (Success)
+ * main - main entry
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * Return: 0
  */
 int main(void)
 {
-	int s, a;
+	int c;
+	int d;
+	int e = 0;
 
-	for (s = 0; s < 100; s++)
+	while (e < 10)
 	{
-		for (a = 0; a < 100; a++)
+		d = 0;
+		while (d < 10)
 		{
-			if (s < a)
+			c = 0;
+			while (c < 10)
 			{
-				putchar((s / 10) + 48);
-				putchar((s % 10) + 48);
-				putchar(' ');
-				putchar((a / 10) + 48);
-				putchar((a % 10) + 48);
-				if (s != 98 || a != 99)
+				if (c != d && d != e && e < d && d < c)
 				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
 
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+				c++;
+			}
+			d++;
 		}
+		e++;
 	}
 	putchar('\n');
-	return (0);
-}
+	return (0);}
